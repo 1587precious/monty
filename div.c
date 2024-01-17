c3aec05ba5d08dccfc3f173887f573cf8579af1a
1587precious <preciousconstance7@gmail.com>
2024-01-17 03:59:40 -0800

    task 0

--- div.c
+++ div.c
@@ -1,51 +1,40 @@
 #include "monty.h"
-
 /**
- * divide_top_two_elements - Divides the top two elements of the stack.
- * @stack_head: Pointer to the stack head
- * @line_number: Line number in the Monty bytecode file
- * Return: No return value
- */
-void divide_top_two_elements(stack_t **stack_head, unsigned int line_number)
+ * f_div - divides the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_div(stack_t **head, unsigned int counter)
 {
-    stack_t *current;
-    int stack_size = 0, result;
+	stack_t *h;
+	int len = 0, aux;
 
-    current = *stack_head;
-
-    while (current)
-    {
-        current = current->next;
-        stack_size++;
-    }
-
-    if (stack_size < 2)
-    {
-        fprintf(stderr, "L%d: cannot divide, stack too short\n", line_number);
-        perform_cleanup_and_exit();
-    }
-
-    current = *stack_head;
-
-    if (current->n == 0)
-    {
-        fprintf(stderr, "L%d: division by zero\n", line_number);
-        perform_cleanup_and_exit();
-    }
-
-    result = current->next->n / current->n;
-    current->next->n = result;
-    *stack_head = current->next;
-    free(current);
-}
-
-/**
- * perform_cleanup_and_exit - Closes file, frees memory, and exits with failure status
- */
-void perform_cleanup_and_exit()
-{
-    fclose(bus.file);
-    free(bus.content);
-    free_stack(*stack_head);
-    exit(EXIT_FAILURE);
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
